1503140881dc397e6b16c7f9d35d88a347362f42
Konashuk <konan4ukv@gmail.com>
2023-11-11 18:28:39 +0200

    remove erors

--- src/components/appi.js
+++ src/components/appi.js
@@ -1,16 +1,16 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-axios.defaults.baseURL = 'https://pixabay.com/api/?';
+// axios.defaults.baseURL = 'https://pixabay.com/api/?';
 
-export const imageFind = async () => {
-  const response = axios.get('', {
-    params: {
-      // q: isValidInputTimeValue,
-      key: '39751957-699f95fae17e6e2f35ebbbaf7',
-      image_type: 'photo',
-      orientation: 'horizontal',
-      per_page: '12',
-    },
-  });
-  return response.hits;
-};
+// export const imageFind = async () => {
+//   const response = axios.get('', {
+//     params: {
+//       // q: isValidInputTimeValue,
+//       key: '39751957-699f95fae17e6e2f35ebbbaf7',
+//       image_type: 'photo',
+//       orientation: 'horizontal',
+//       per_page: '12',
+//     },
+//   });
+//   return response.hits;
+// };
