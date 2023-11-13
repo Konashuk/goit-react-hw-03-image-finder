3acaa7fe05df6becee6c6b00ca304640f385de35
Konashuk <konan4ukv@gmail.com>
2023-11-13 16:23:25 +0200

    add notification

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import Notiflix from 'notiflix';
 import { SearchBar } from './search-bar/searchBar';
 import { ImageGalery } from './image-galery/imageGallery';
 import { Loader } from './laoder';
@@ -32,15 +33,28 @@ class App extends Component {
   };
 
   fetchImages = async () => {
+    const splitQuery = this.state.query.split(/\/(.*)/)[1].trim();
+
+    if (!splitQuery) {
+      Notiflix.Notify.warning('Please enter a what search.');
+      return;
+    }
     try {
       this.setState({ isLoading: true });
-      const splitQuery = this.state.query.split(/\/(.*)/)[1];
       const { totalHits, hits } = await imageFind(splitQuery, this.state.page);
+      if (hits.length === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+
+        return;
+      }
       this.setState(prevState => ({
         images: [...prevState.images, ...hits],
         loadMore: this.state.page < Math.ceil(totalHits / 12),
       }));
     } catch (error) {
+      Notiflix.Notify.warning('Sorry, somesing weree wrong. Try later🙏');
     } finally {
       this.setState({ isLoading: false });
     }
